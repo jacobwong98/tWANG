7b79c7f7b3e4f93a575e4d442384648ff1d4f87e
Phlana <joshua.chang422@gmail.com>
2020-01-18 17:49:07 -0700

    Update Player_Movement.gd

--- Scripts/Player_Movement.gd
+++ Scripts/Player_Movement.gd
@@ -9,38 +9,64 @@ func _process(delta):
 	pass
 
 # export (int) var move_speed = 800
-const MOVE_SPEED = 400
-const JUMP_FORCE = 900
-const GRAVITY = 60
+const MAX_MOVE_SPEED = 400
 const MAX_FALL_SPEED = 200
+const ACCELERATION = 70
+const DECELERATION = 70
+const GRAVITY = 60
+const JUMP_FORCE = 900
+
 
 var velocity = Vector2()
-var accel
+var accel = 0
 var double_jump_flag = false
+
 func get_input():
 	var grounded = is_on_floor()
+	var walled = is_on_wall()
 	var bonked = is_on_ceiling()
-	# reset horizontal velocity
-	velocity.x = 0
 	
 	# left and right movement
 	if Input.is_action_pressed("right"):
-		velocity.x = MOVE_SPEED
+		velocity.x += ACCELERATION
 	if Input.is_action_pressed("left"):
-		velocity.x = -MOVE_SPEED
+		velocity.x -= ACCELERATION
+	
+	# cap horizontal movement
+	if velocity.x > MAX_MOVE_SPEED:
+		velocity.x = MAX_MOVE_SPEED
+	if velocity.x < -MAX_MOVE_SPEED:
+		velocity.x = -MAX_MOVE_SPEED
+		
+	# neither left nor right is being pressed for deceleration
+	if not Input.is_action_pressed("right") and not Input.is_action_pressed("left"):
+		if velocity.x > 0:
+			velocity.x -= DECELERATION
+			if velocity.x < 0:
+				velocity.x = 0
+		if velocity.x < 0:
+			velocity.x += DECELERATION
+			if velocity.x > 0:
+				velocity.x = 0
+	
+	# reset horizontal movement if touching wall
+	if walled:
+		velocity.x = 0
 	
 	# reset double jump when on the ground
 	if grounded:
 		velocity.y = 0
 		double_jump_flag = false
 		
-	# jump
+	# space pressed to jump
 	if not double_jump_flag and Input.is_action_just_pressed('jump'):
 		velocity.y = -JUMP_FORCE
 		
+		# arial jump used, change flag
 		if not grounded:
 			double_jump_flag = true
-			
+	
+	# escape pressed to exit
 	if Input.is_action_pressed('quit'):
 		get_tree().quit()
 	
@@ -53,5 +79,5 @@ func get_input():
 
 func _physics_process(delta):
 	get_input()
-
-	move_and_slide(velocity, Vector2(0, -1))
+	# execute movement
+	var slide = move_and_slide(velocity, Vector2(0, -1))
