04cabbf7e7115f9710e93be7c3af53f4cc68b484
rahulminy <rahulnayyar11@gmail.com>
2020-01-18 19:34:43 -0700

    Added working targets

---
+++ Target.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Area2D.connect("area_entered", self, "_on_Area2D_area_entered")
+	
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+func _on_Area2D_area_entered(area):
+	if area.name == "ArrowHitbox":
+		emit_signal("target_broken")
+		queue_free()
+	pass # Replace with function body.
